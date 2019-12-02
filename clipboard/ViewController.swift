859633dccbffe31cda85d7a0de2dd6f5d449a45e
AdrianQaQ <adrianqaq777@gmail.com>
2019-12-02 15:05:39 +0800

    Initial Commit

---
+++ clipboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  clipboard
+//
+//  Created by wyy on 2019/12/2.
+//  Copyright © 2019 yahaha. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
